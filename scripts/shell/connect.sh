a312711e2b74e587a60aacee3e5aa4c727c78271
Zakir Hussain <itsjakirhussain@gmail.com>
2022-06-30 23:41:34 +0530

    fix

--- scripts/shell/connect.sh
+++ scripts/shell/connect.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 
 echo "Deploying to ${DEPLOY_SERVER_USER}@${DEPLOY_SERVER_URL}"
-ssh ${DEPLOY_SERVER_USER}@${DEPLOY_SERVER_URL} 'bash' < ./scripts/deploy.sh
+ssh ${DEPLOY_SERVER_USER}@${DEPLOY_SERVER_URL} 'bash' < ./scripts/shell/deploy.sh
